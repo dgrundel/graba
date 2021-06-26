f58059a5246c87d03533a91201429cd3ddb26af7
grundel <daniel@grundel.me>
2021-06-25 17:14:41 -0700

    finding start and end of jpgs

--- packages/prototype/ffmpeg.js
+++ packages/prototype/ffmpeg.js
@@ -2,31 +2,55 @@ const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
 const sharp = require('sharp');
+const os = require('os');
 
 const QUALITY_LEVEL = 24;
-const MAX_FRAME_RATE = 20;
+const MAX_FRAME_RATE = 10;
 
-const sourceUrl = 'rtsp://user:pass@ipaddr:8554/unicast';
+const sourceUrl = require('./secrets.json').feeds[1];
+const outputDir = path.resolve('./output');
 
-const ffmpegPath = path.resolve(__dirname, 'bin/ffmpeg.exe');
-const ff = child_process.spawn(ffmpegPath, [
-    '-i', sourceUrl,
-    '-filter:v', `fps='fps=min(${MAX_FRAME_RATE},source_fps)'`,
-    '-f', 'image2', // force output format to image
-    '-c:v', 'mjpeg', // format output to jpeg
-    '-qscale:v', QUALITY_LEVEL, // quality, 1-31 where 1 is best
-    '-update', '1', // keep writing to the same output (pipe/stdout in this case)
-    'pipe:1', // output to stdout
-]);
+const JPG_START = 'ffd8';
+const JPG_END = 'ffd9';
+
+const ffmpegArgs = [
+    '-i', sourceUrl, // input
+    '-filter:v', `fps='fps=min(${MAX_FRAME_RATE},source_fps)'`, // set max fps
+    '-f', 'image2', // use image processor
+    '-c:v', 'mjpeg', // output a jpg
+    '-qscale:v', QUALITY_LEVEL.toString(), // set quality level
+    // '-frames:v', '1', // output a single frame
+    '-update', '1', // reuse the same output (stdout in this case)
+    'pipe:1', // pipe to stdout
+];
+const ff = child_process.spawn('ffmpeg', ffmpegArgs);
+
+console.log(ffmpegArgs.join(' '));
+
+let fileStream;
 
 ff.stdout.on('data', data => {
-    // console.log('stdout', data);
+    const startBytes = data.slice(0, 2).toString('hex');
+    if (startBytes === JPG_START) {
+        fileStream = fs.createWriteStream(path.join(outputDir, `output-${+new Date()}.jpg`));
+    }
+    
+    // console.log('contains EOL', data.toString().indexOf(os.EOL));
 
     // should handle rejection here and skip
     // frame if need be
-    sharp(data)
-        .jpeg()    
-        .toFile(`output-${+new Date()}.jpg`);
+    // sharp(data, { sequentialRead: true })
+    //     .ensureAlpha()
+    //     .jpeg()
+    //     .toFile(path.join(outputDir, `output-${+new Date()}.jpg`))
+    //     .catch(err => console.error(err));
+
+    fileStream.write(data);
+
+    const endBytes = data.slice(-2).toString('hex');
+    if (endBytes === JPG_END) {
+        fileStream.end();
+    }
 });
 
 ff.stderr.on('data', data => {
