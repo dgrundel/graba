a901dfa297fad42c37253d8fb7e5748e923d9d43
grundel <daniel@grundel.me>
2021-08-15 14:16:59 -0700

    upload to cloudinary as private, generate expiring link

--- packages/server/src/helpers/cloudinary.ts
+++ packages/server/src/helpers/cloudinary.ts
@@ -11,45 +11,36 @@ const preconfigure = () => {
 };
 
 const ONE_MINUTE = 1000 * 60;
-const UPLOAD_PUBLIC_TIMEOUT = ONE_MINUTE * 15;
+const PUBLIC_URL_TIMEOUT = ONE_MINUTE * 15;
 
-export const uploadImage = (image: string): Promise<string> => {
-    return new Promise((resolve, reject) => {
-        preconfigure();
+export const uploadImage = async (image: string): Promise<string> => {
+    preconfigure();
+        
+    const uploadOptions: cloudinary.UploadApiOptions = {
+        type: "private",
+    };
+    
+    logger.debug('Uploading image to Cloudinary with options', uploadOptions);
 
-        const callback: cloudinary.UploadResponseCallback = (err?: cloudinary.UploadApiErrorResponse, result?: cloudinary.UploadApiResponse): void => {
-            if (err) {
-                logger.error('Error returned from Cloudinary', err);
-                reject(err);
-                return;
-            }
+    const uploadResult = await cloudinary.v2.uploader.upload(image, uploadOptions);
 
-            if (!result) {
-                const message = 'No result returned from Cloudinary';
-                logger.error(message);
-                reject(new Error(message));
-                return;
-            }
-
-            logger.debug('Cloudinary upload successful', {
-                assetId: result.asset_id,
-                publicId: result.public_id,
-                url: result.url,
-                secureUrl: result.secure_url,
-            });
-
-            resolve(result.secure_url);
-        };
-
-        const options: cloudinary.UploadApiOptions = {
-            access_type: "anonymous",
-            start: (new Date()).toISOString(),
-            end: (new Date(Date.now() + UPLOAD_PUBLIC_TIMEOUT)).toISOString(),
-        };
-
-        logger.debug('Uploading image to Cloudinary with options', options);
-
-        cloudinary.v2.uploader.upload(image, options, callback);
+    logger.debug('Cloudinary upload successful', {
+        assetId: uploadResult.asset_id,
+        publicId: uploadResult.public_id,
+        url: uploadResult.url,
+        secureUrl: uploadResult.secure_url,
     });
+
+    const expiresTimestampSeconds = (Date.now() + PUBLIC_URL_TIMEOUT) / 1000;
+
+    const url = cloudinary.v2.utils.private_download_url(uploadResult.public_id, uploadResult.format, {
+        expires_at: expiresTimestampSeconds,
+        resource_type: uploadResult.resource_type,
+        attachment: false,
+    });
+
+    logger.debug('Private image URL generated: ' + url);
+
+    return url;
 };
     
